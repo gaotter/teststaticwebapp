6c90240b3a4488f08f388e4ae82fe58c52cddba1
gaotter <gaotter@gmail.com>
2020-05-21 22:34:30 +0200

    test width api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './service/data.service';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `<div>Hello {{(nameMessage | async).Name}}</div>`,
 })
-export class AppComponent {
+export class AppComponent  {
+
+  nameMessage = this.dataService.getName();
+constructor(private dataService:DataService) {}
+
+
+
   value = 'World';
 }
